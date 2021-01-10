b5449b3cb9892b91e2d41da3eb67149e37ef9d02
Naofal EL Ftouh <naofal.elftouh@routz.nl>
2021-01-10 20:51:39 +0100

    ExerciseEvalu.py

---
+++ ExerciseEvalu.py
@@ -0,0 +1,14 @@
+Naofal = 32
+Chaimae = 36
+Loujain = 8
+Abdelaziz = 5
+
+print("Naofal is", Naofal - Chaimae, "Jaar jonger dan Chaimae")
+print("Loujain is", Loujain - Abdelaziz, "Jaar ouder dan Abdelaziz ")
+
+leeftijd = input("Hoe oud is Chaimae?")
+print("Chaimae is", leeftijd, "Oud!!"36
+      )
+
+name = input("What is your name? ")
+print("Welcome abord", name, "!")

63bea6b86c3de5f7074bf56e5bc1689840a71bec
Naofal EL Ftouh <naofal.elftouh@routz.nl>
2021-01-12 21:25:03 +0100

    Exercise5.py

--- Exercise5.py
+++ Exercise5.py
@@ -1,16 +1,19 @@
-my_name = 'Naofal'
-my_age = 32 # not a lie
-my_height = 1.76  # m
-my_weight = 80 # kg
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Naofal'
+age = 32 # not a lie
+height = 1.76  # m
+weight = 80 # kg
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+centimeter = height * 100
+gram = weight * 100
 
-print(f"Let's talk about {my_name}.")
-print(f"He's {my_height} m tall")
-print(f"He's {my_weight} Kg heavy.")
+print(f"Let's talk about {name}.")
+print(f"He's {height} m tall")
+print(f"He's weight in gram is {gram} and he's height in Cm is {centimeter}")
+print(f"He's {weight} Kg heavy.")
 print("Actually that's not too heavy.")
-print(f"He's got {my_eyes} eyes and {my_hair} hair.")
-print(f"His teeth are usually {my_teeth} depending on the coffee.")
-total = my_age + my_height + my_weight
-print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+total = age + height + weight
+print(f"If I add {age}, {height} and {weight} I get {total}.")
---
+++ ExerciseEvalu1.py
@@ -0,0 +1,19 @@
+soort_mensen = 10
+x = f"Er zijn {soort_mensen} mensen."
+roken = "Die roken"
+niet_roken = "Die niet roken"
+y = f"Er zijn ook veel {roken} en weinig {niet_roken}."
+
+print(x)
+print(y)
+
+print(f"Ik zei {x}.")
+print(f"Ik zei ook {y}.")
+hilarisch = "Hola"
+grap_evaluatie = "Is deze grap niet grapig?! {} "
+print(grap_evaluatie.format(hilarisch))
+w = "Dit is de linkerkant van ..."
+e = "Dit is de rechterkant van de string"
+
+print(w+e)
+

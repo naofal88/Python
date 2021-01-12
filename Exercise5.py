af079298960a7d07c8f9baa027411aaaf0e624f8
Naofal EL Ftouh <naofal.elftouh@routz.nl>
2021-01-12 20:29:57 +0100

    Exercise5.py

---
+++ Exercise5.py
@@ -0,0 +1,16 @@
+my_name = 'Naofal'
+my_age = 32 # not a lie
+my_height = 1.76  # m
+my_weight = 80 # kg
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} m tall")
+print(f"He's {my_weight} Kg heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}.")
--- ExerciseEvalu.py
+++ ExerciseEvalu.py
@@ -7,7 +7,7 @@ print("Naofal is", Naofal - Chaimae, "Jaar jonger dan Chaimae")
 print("Loujain is", Loujain - Abdelaziz, "Jaar ouder dan Abdelaziz ")
 
 leeftijd = input("Hoe oud is Chaimae?")
-print("Chaimae is", leeftijd, "Oud!!"36
+print("Chaimae is", leeftijd, "Oud!!"
       )
 
 name = input("What is your name? ")

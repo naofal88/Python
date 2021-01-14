7cc1eaaf097b9b6995be4c6a89570cc88d13ee11
Naofal EL Ftouh <naofal.elftouh@routz.nl>
2021-01-14 18:56:59 +0100

    Exercise8.py

---
+++ Exercise8.py
@@ -0,0 +1,8 @@
+ 
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format("Naofal", "Reda", "Mohamed", 3))
---
+++ Exercise9.py
@@ -0,0 +1,12 @@
+formatter = "{} {} {} {} {}"
+days = "Mon Tue Wed Thu Fri Sat Sun "
+months = "\nJan\nFeb\nMar\nApr\nMar\nApr\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print(formatter.format("Here are the days ", days, "Hola", 1, 5))
+print("Here are the months", months)
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to print as much as we like.
+Even 4 lines if we want, or 5, or 6
+""")

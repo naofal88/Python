82166e4ea51c6b1738b096449116ea7c5f5eee8b
Naofal EL Ftouh <naofal.elftouh@routz.nl>
2021-01-15 20:50:20 +0100

    ExerciseEvalu2.py

--- ExerciseEvalu2.py
+++ ExerciseEvalu2.py
@@ -14,3 +14,7 @@ ASCII_LF = "\nEsto \nte \nenvia\n hacia \n abajo"
 print(ASCII_LF)
 carriage_return = "\rEsto es\n lo que \rhay"
 print(carriage_return)
+horizontal_tab = "\ty esto \tque va a \thacer ahora"
+print(horizontal_tab)
+ASCII_VT= "\vesto \vhace \vuna \vtab vertical "
+print(ASCII_VT)

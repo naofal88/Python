2e5702e1a30b5b5015384eaef5a06fe9f026d86b
Naofal EL Ftouh <naofal.elftouh@routz.nl>
2021-01-15 20:46:28 +0100

    ExerciseEvalu2.py

---
+++ ExerciseEvalu2.py
@@ -0,0 +1,16 @@
+backslash = "\\"
+print(backslash)
+single_quote = "\'"
+print(single_quote)
+double_quote = "\""
+print(double_quote)
+ASCII_BEL = "\aBell"
+print(ASCII_BEL)
+ASCII_BS = "\baqui \bsiempre \bse \belimina la ultima letra" #\b Elimina la ultima letra
+print(ASCII_BS)
+ASCII_FF = "\fA ver \f lo \f que \f hace esto"
+print(ASCII_FF)
+ASCII_LF = "\nEsto \nte \nenvia\n hacia \n abajo"
+print(ASCII_LF)
+carriage_return = "\rEsto es\n lo que \rhay"
+print(carriage_return)

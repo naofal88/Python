6b4eed1485065cdf876313b9f32ac09828e2b045
Naofal EL Ftouh <naofal.elftouh@routz.nl>
2021-01-21 20:12:56 +0100

    Exercise12.py

---
+++ Exercise12.py
@@ -0,0 +1,5 @@
+age = input("How old are you ?")
+weight = input(f"are you {age}? and what is your weight?")
+height = input("How tall are you ?")
+
+print(f"So you are {age} old, {weight} heavy and {height} tall")

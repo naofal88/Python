d3b1fd0ea97b6964fddec46aa732279b35aa92f9
Naofal EL Ftouh <naofal.elftouh@routz.nl>
2021-01-24 19:23:47 +0100

    Exercise13.py

---
+++ ExerciseEvalu3.py
@@ -0,0 +1,12 @@
+print("Wat is je naam?", end=' ')
+naam = input()
+print("How old are you ?", end=' ')
+age = input()
+print("How tall are you ?", end=' ')
+height = input()
+print("How much do you weight?", end=' ')
+weight = input()
+
+print(f"So je naam is {naam} you are {age}, {height} tall and Loujain{weight} heavy")
+
+print("Hi I'm Abdelaziz, Nice to meet you Abdelaziz")

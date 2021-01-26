05fe0a59f8aed62f6ce50ad44194df96bcf3d996
Naofal EL Ftouh <naofal.elftouh@routz.nl>
2021-01-26 21:01:38 +0100

    Exercise14.py

---
+++ Exercise14.py
@@ -0,0 +1,20 @@
+from sys import argv
+script, user_name, name, password = argv
+prompt = '>'
+
+print(f"Hi {user_name}, I'm the {script} script.")
+
+print("I'd like to ask you a few questions.")
+print(f"Do you like my {user_name}?")
+likes = input(prompt)
+print(f"I heard that your password is {password}")
+password = input(prompt)
+print(f"Where do you live {name}?")
+lives = input(prompt)
+print("What kind of computer do you have?")
+computer = input(prompt)
+print(f""""
+alright, so you said {likes} about liking me.
+You live in {lives}. Not sure where that is. 
+And you have a {computer} computer with {password} as password. Nice.
+""")
